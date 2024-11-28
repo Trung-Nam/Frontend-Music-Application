e6183cd2622f2dd9df26ef09372b8a090b8b66f4
Trung Nam <trungnam.work@gmail.com>
2024-11-29 01:16:05 +0700

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-red-500 font-bold'>Hello</h1>
+      <Button variant={"destructive"}>This is a button</Button>
+    </>
+  )
+}
+
+export default App
