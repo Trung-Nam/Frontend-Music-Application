c625796fe320a763fe9763be2628126810ed1080
Trung Nam <trungnam.work@gmail.com>
2024-12-01 23:46:34 +0700

    Responsive for mobile

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -8,9 +8,20 @@ import LeftSidebar from "./components/LeftSidebar";
 import FriendsActivity from "./components/FriendsActivity";
 import AudioPlayer from "./components/AudioPlayer";
 import PlaybackControls from "./components/PlaybackControls";
+import { useEffect, useState } from "react";
 
 const MainLayout = () => {
-  const isMobile = false;
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    checkMobile();
+    window.addEventListener("resize", checkMobile);
+    return () => window.removeEventListener("resize", checkMobile);
+  }, []);
 
   return (
     <div className="h-screen bg-black text-white flex flex-col">
@@ -18,14 +29,14 @@ const MainLayout = () => {
         direction="horizontal"
         className="flex flex-1 h-full overflow-hidden p-2"
       >
-        <AudioPlayer/>
+        <AudioPlayer />
         {/* Left sidebar */}
         <ResizablePanel
           defaultSize={20}
           minSize={isMobile ? 0 : 10}
           maxSize={30}
         >
-          <LeftSidebar/>
+          <LeftSidebar />
         </ResizablePanel>
 
         <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
@@ -34,20 +45,25 @@ const MainLayout = () => {
         <ResizablePanel defaultSize={isMobile ? 80 : 60}>
           <Outlet />
         </ResizablePanel>
-        <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
 
-        {/* Right sidebar */}
-        <ResizablePanel
-          defaultSize={20}
-          minSize={0}
-          maxSize={25}
-          collapsedSize={0}
-        >
-          <FriendsActivity/>
-        </ResizablePanel>
+        {!isMobile && (
+          <>
+            <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
+
+            {/* Right sidebar */}
+            <ResizablePanel
+              defaultSize={20}
+              minSize={0}
+              maxSize={25}
+              collapsedSize={0}
+            >
+              <FriendsActivity />
+            </ResizablePanel>
+          </>
+        )}
       </ResizablePanelGroup>
 
-      <PlaybackControls/>
+      <PlaybackControls />
     </div>
   );
 };
