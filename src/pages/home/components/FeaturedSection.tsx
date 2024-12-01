82080b4ef27d77fa6d91b9c6c4e78a17ed4d3021
Trung Nam <trungnam.work@gmail.com>
2024-12-01 22:44:42 +0700

    Play button component

--- src/pages/home/components/FeaturedSection.tsx
+++ src/pages/home/components/FeaturedSection.tsx
@@ -1,5 +1,6 @@
 import FeaturedGridSkeleton from "@/components/skeletons/FeaturedGridSkeleton";
 import { useMusic } from "@/stores/useMusic";
+import PlayButton from "./PlayButton";
 
 const FeaturedSection = () => {
   const { isLoading, featuredSongs, error } = useMusic();
@@ -28,9 +29,8 @@ const FeaturedSection = () => {
             <p className="font-medium truncate">{song?.title}</p>
             <p className="text-sm text-zinc-400 truncate">{song?.artist}</p>
           </div>
+          <PlayButton song={song} />
         </div>
-
-        // todo: add play button
       ))}
     </div>
   );
--- src/pages/home/components/SectionGrid.tsx
+++ src/pages/home/components/SectionGrid.tsx
@@ -1,6 +1,7 @@
 import SectionGridSkeleton from "@/components/skeletons/SectionGridSkeleton";
 import { Button } from "@/components/ui/button";
 import { Song } from "@/types";
+import PlayButton from "./PlayButton";
 
 type SectionGridProps = {
   title: string;
@@ -37,6 +38,7 @@ const SectionGrid = ({ title, songs, isLoading }: SectionGridProps) => {
                   className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
                 />
                 {/* todo: add play btn */}
+                <PlayButton song={song}/>
               </div>
             </div>
             <h3 className="font-medium mb-2 truncate">{song?.title}</h3>
