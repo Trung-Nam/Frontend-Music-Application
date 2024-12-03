1f39fc29f967c3be1de236e2a273328412205217
Trung Nam <trungnam.work@gmail.com>
2024-12-03 10:49:10 +0700

    Send message and real time online status

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -3,3 +3,11 @@ export const formatDuration = (seconds: number) => {
   const remainingSeconds = Math.floor(seconds % 60);
   return `${minutes}:${remainingSeconds.toString().padStart(2, "0")}`;
 };
+
+export const formatTime = (date: string) => {
+	return new Date(date).toLocaleTimeString("en-US", {
+		hour: "2-digit",
+		minute: "2-digit",
+		hour12: true,
+	});
+};
