f1cc1c873fcd1fd180003d29de2ffff2f51fca23
Trung Nam <trungnam.work@gmail.com>
2024-12-03 12:54:16 +0700

    update origin url #2

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
-import axios from "axios"
+import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "https://backend-music-flow-application.onrender.com",
+  baseURL: "https://backend-music-flow-application.onrender.com/api",
 });
